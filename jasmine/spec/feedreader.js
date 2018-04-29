e3ea41e4a645e848e9786510f308de5b4df3f9b5
Tenacious-G <gordona86550@gmail.com>
2018-04-29 19:31:38 +0100

    initial feed reader spec (14) completed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -108,15 +108,24 @@ $(function() {
 	});
 });
     /* TODO: Write a new test suite named "Initial Entries" */
-$(function() {
-    describe('Initial Entries', function() {
+$(function(){
+    describe('Initial Entries', function(){
 		/* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-		it('is hidden by default', function() {
+
+		beforeEach(function(done){
+			loadFeed(3, done())(function(){
+				 done();
+			 });
+		});
+		
+		it('there is at least a single .entry element within the .feed container.', function(done) {
+			expect(allFeeds[0]).not.toBe(null);
+			done();
 		});	
 	});
 });		
