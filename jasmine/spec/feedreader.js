8b438c80631ea57f0a43240fff69cc8c1a4b5a87
Tenacious-G <gordona86550@gmail.com>
2018-05-02 21:34:50 +0100

    16 content changes when a new feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,16 +27,11 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
 		it('has a URL defined', function(){
-			//expect(allFeeds.forEach(url)).toBeDefined();
-				// forEach(url in allFeeds){
-					// expect(allFeeds(url)).toBeDefined();
-				// }
-			//for in loops with key value pairs
 			//adapted from stack overflow https://stackoverflow.com/questions/7241878/for-in-loops-in-javascript-key-value-pairs?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa			
 			for (let url in allFeeds){
 				let value = allFeeds[url];
@@ -51,7 +46,7 @@ $(function() {
 			}
 		});
 
-        /* TODO: Write a test that loops through each feed
+        /* a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -72,13 +67,13 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* a new test suite named "The menu" */
 $(function() {
     let menuIcon = $('.menu-icon-link');
 	//let body = $('.menu-hidden');
 	
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
+        /* a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -90,7 +85,7 @@ $(function() {
 			expect(menuHidden).toBeDefined();			
 		});
 		
-         /* TODO: Write a test that ensures the menu changes
+         /* a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -107,10 +102,10 @@ $(function() {
 		});		
 	});
 });
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* a new test suite named "Initial Entries" */
 $(function(){
     describe('Initial Entries', function(){
-		/* TODO: Write a test that ensures when the loadFeed
+		/* a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -129,10 +124,34 @@ $(function(){
 		});	
 	});
 });		
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* a new test suite named "New Feed Selection" */
+$(function(){
+    describe('New Feed Selection', function(){
+        /* a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+		//take a snapshot of the feed array before the loadFeed function is called
+		let startingFeeds = [];
+		let finishingFeeds = [];
+
+		beforeEach(function(done){
+			loadFeed(0, done())(function(){
+				startingFeeds = allFeeds;
+				done();
+			});
+		});
+		
+		it('content has changed.', function(done) {//runs when "done" is returned from the beforeEach function
+			let cb;
+			//second load
+			loadFeed(0,cb);
+			finishingFeeds = allFeeds;
+			//check after asynchronous function loadFeed has run
+			expect(startingFeeds).not.toEqual(finishingFeeds);
+			done();
+		});	
+	});
+});	
 }());
